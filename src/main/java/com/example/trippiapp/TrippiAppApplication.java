ab6ddf708cd29cde86ca3c8e3e31181ef0a958e2
Robert Wyszynski <robertointerwento@gmail.com>
2026-03-06 21:55:42 +0100

    first commit

---
+++ src/main/java/com/example/trippiapp/TrippiAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.trippiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrippiAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrippiAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trippiapp/TrippiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trippiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrippiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
