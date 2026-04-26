f594c052ed3082c26845cfbd5ee81e4b40434134
Robert Wyszynski <robertointerwento@gmail.com>
2026-04-26 23:36:30 +0200

    change in cors

--- chat-service/src/main/java/com/example/chat/config/SecurityConfig.java
+++ chat-service/src/main/java/com/example/chat/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(
-                List.of("http://localhost:4200", "https://rwyszynski.github.io"));
+                List.of("http://localhost:4200", "https://rwyszynski.github.io", "https://cerulean-manatee-609fd0.netlify.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
